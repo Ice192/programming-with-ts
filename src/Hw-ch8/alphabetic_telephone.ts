e8e26eb5d19943486d6d4e685fd021bbe4513ade
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-05-04 17:13:58 +0800

    update

--- src/Hw-ch8/alphabetic_telephone.ts
+++ src/Hw-ch8/alphabetic_telephone.ts
@@ -3,10 +3,23 @@ import { question } from "readline-sync";
 const valid_char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-"
 const groups = ['aAbBcC','dDeEfF','gGhHiI','jJkKlL','mMnNoO','pPqQrRsS','tTuUvV','wWxXyYzZ']
 
+function isThere (chars: string, char: string): boolean{
+    let result = false
+    for(const huruf of chars){
+        if(huruf === char){
+            result = true
+    }
+    }
+    return result
+}
+
 function transform (char:string): number {
     let number = 0
     for(const index in groups){
-        if(groups[index]?.includes(char)){
+        if(groups[index]===undefined){
+            throw new Error ("Invalid value")
+        }
+        if(isThere(groups[index], char)){
             number += Number(index) + 2
         }
     }
@@ -17,7 +30,7 @@ const input = question("Input telephone with format XXX-XXX-XXXX : ")
 let print = ''
     if(input.length === 12 && input[3] === "-" && input[7] === "-"){
         for(const char of input){
-            if(valid_char.includes(char)){
+            if(isThere(valid_char,char)){
                 const number_char = Number(char)
                 if(char === '-' || !isNaN(number_char)){                   
                     print += char
