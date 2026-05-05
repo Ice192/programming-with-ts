7f80897a3b1bb8d4ac6e94abdd7ce2583edcec72
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-05-05 13:11:55 +0800

    initial

--- src/Hw-ch8/initial.ts
+++ src/Hw-ch8/initial.ts
@@ -1,22 +1,61 @@
+import { question } from "readline-sync"
 
-// const first = "Jhon"
-// const second = "William"
-// const last = "Smith"
-
-// console.log(`${first} ${second} ${last}`);
-// console.log(`Initial ${first[0]}.${second[0]}.${last[0]}.`);
-
-
-const fullName = "Jhon William Smith"
-const name = fullName.split(" ")
-
-const first = name[0]
-const second = name[1]
-const last = name[2]
-
-if(first === undefined || second === undefined || last === undefined){
-    throw new Error("Nama tidak Lengkap")
+function splitName (fullname: string) : string[]{
+    const result = []
+    let print = ''
+    for(const huruf of fullName){        
+        if(huruf !== " "){
+            print += huruf
+        } else if(print !== ''){
+            result.push(print)
+            print = ''
+        } else {
+            print = ''
+        }
+    }
+    if(print !== ''){
+            result.push(print)
+        }
+    return result
 }
 
-console.log(fullName);
-console.log(`Initial ${first[0]}.${second[0]}.${last[0]}.`);
+function toUpper(huruf:string):string{
+    const huruf_kecil = "abcdefghijklmnopqrstuvwxyz"
+    const huruf_besar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    for(let i = 0; i < huruf_kecil.length; i++){
+        const kecil = huruf_kecil[i]
+        if( kecil === huruf){
+            const convert = huruf_besar[i]
+            if(convert === undefined){
+                return huruf
+             }
+            return convert
+        } 
+    }
+
+    return huruf
+    
+}
+
+
+function takeInitial (name: string[]): string{
+    let print = ''
+    for(const nama of name){
+        const initial = nama[0]
+
+        if(initial === undefined){
+            throw new Error("Undefined value")
+        }
+
+        print += `${toUpper(initial)}.`
+    }
+    return print
+}
+
+const fullName = question("Input your fullname : ")
+
+console.log(splitName(fullName));
+
+const name = splitName(fullName)
+console.log(takeInitial(name));
